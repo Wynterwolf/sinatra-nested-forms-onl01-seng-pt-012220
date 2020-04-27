1c0820134ca375aac79fdc58a94ac7ec69217e67
Samantha Jordan <shinyguns@gmail.com>
2020-04-27 21:02:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,24 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
-
+    get '/' do
+      erb :root 
+    end
+    
+    get '/new' do
+      erv :'pirates/new'
+    end
+    
+    post "/pirates" do
+      @pirate = Pirate.new(params[:pirate])
+      
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.assert_select_email
+      
+      erb :'pirates/show'
+    end
+        
   end
 end
